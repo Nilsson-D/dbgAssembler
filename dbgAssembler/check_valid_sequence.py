f8cebff50d378911367ee4115081c38240a9441b
Daniel-N <da0260ni-s@student.lu.se>
2022-03-10 10:50:03 +0100

    add comments and remove class

--- dbgAssembler/check_valid_sequence.py
+++ dbgAssembler/check_valid_sequence.py
@@ -1,63 +1,77 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
-Created on Mon Feb 28 08:03:55 2022
+Title: check_valid_sequence.py
+Created on Mon 07-03-2022
+Author: Daniel Nilsson
 
-@author: daniel
+Description:
+    A script to store helper functions for the dbgAssembler regarding 
+    checking if the input sequence is valid. 
+
+List of functions:
+    check_valid_sequence()
+             
+   
+Procedure:
+    1. get the sequence, the type, and if the sequence is from an alignment
+    2. check whatever the sequence contains invalid characters
+    3. return a boolean whatever the sequence is valid or not
+    
+Usage:
+    - 
+    used as module
 """
 
-import re
-
-#script for reading fasta files
-
-
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-class WrongFormat(Error):
-    """Raised when the input is in wrong format"""
-    pass
-
+import re #allows searching for invalid characters 
 
 
 
 def check_valid_sequence(fasta_line, seq_type = "DNA", isAligned = False):
-    seq_type = seq_type.upper()
-    valid_fasta = True
+    """
+    Check whatever a dna, rna or protein sequence has the correct characters.
+    It should be specified if the sequence is from an alignment 
+    """
+    seq_type = seq_type.upper() #seq_type to uppercase so this wont create a problem
+    valid_fasta = True #create a boolean to return whatever the string/sequence has valid characters
     
     
-    if seq_type == "DNA":
+    if seq_type == "DNA": #if DNA is specified
+        #check whatever the sequence is from an alignment
         if isAligned:         
             valid_fasta_chrs = r"[^ACGT-]+"
         else:
             valid_fasta_chrs = r"[^ACGT]+"
             
-            
-        result = re.findall(valid_fasta_chrs, fasta_line.strip(), re.IGNORECASE)
+         #search for all characters except valid_fasta_chrs    
+        result = re.findall(valid_fasta_chrs, fasta_line.strip(), re.IGNORECASE) #ignore case when searching
         if result:
-            valid_fasta = False
+            valid_fasta = False #return true if any other chr is found
             
         
-    elif seq_type == "PROTEIN": 
+    elif seq_type == "PROTEIN":  #if protein is specified
+    #check whatever the sequence is from an alignment
         if isAligned: 
             valid_fasta_chrs = r"[^ABCDEFGHIJKLMNOPQRSTUVWYZX*-]+"
         else:
             valid_fasta_chrs = r"[^ABCDEFGHIJKLMNOPQRSTUVWYZX*]+"
             
-        result = re.findall(valid_fasta_chrs, fasta_line.strip(), re.IGNORECASE)
+        #search for all characters except valid_fasta_chrs    
+        result = re.findall(valid_fasta_chrs, fasta_line.strip(), re.IGNORECASE) #ignore case when searching
         if result:
-            valid_fasta = False
+            valid_fasta = False #return true if any other chr is found
 
-        
-    elif seq_type == "RNA":
+    
+    elif seq_type == "RNA": #if RNA is specified
+        #check whatever the sequence is from an alignment
         if isAligned:         
             valid_fasta_chrs = r"[^ACGU-]+"
         else:
             valid_fasta_chrs = r"[^ACGU]+"
-    
-        result = re.findall(valid_fasta_chrs, fasta_line.strip(), re.IGNORECASE)
+            
+        #search for all characters except valid_fasta_chrs
+        result = re.findall(valid_fasta_chrs, fasta_line.strip(), re.IGNORECASE) #ignore case when searching
         if result:
-            valid_fasta = False
+            valid_fasta = False #return true if any other chr is found
     
-    return valid_fasta
+    return valid_fasta #return boolean
