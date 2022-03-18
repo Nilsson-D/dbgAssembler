eedc0187734b71cbc065ca642a7fa0f99f09c090
Daniel-N <da0260ni-s@student.lu.se>
2022-03-18 12:16:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,4 @@ setup(
     description="Simple genome assembler",
     packages=find_packages(),
     python_requires="3.9.10",
-    install_requires=[
-      "pygraphviz==1.9",
-      "pandas==1.4.1",
-      "numpy==1.22.2",
-      "networkx==2.5",
-      "matplotlib==3.5.1"
-    ]
-
 )
