e34799acef8ed42054f3ee93738b572dcf99c531
Daniel-N <da0260ni-s@student.lu.se>
2022-03-18 12:15:42 +0100

    Remove test result and unnecessary scripts

--- test/test_run_viruses.py
+++ test/test_run_viruses.py
@@ -11,7 +11,6 @@ List of functions:
     None
     
 List of "non standard" modules:
-    - difflib
 
    
 Procedure:
@@ -33,8 +32,7 @@ from Assembler.Utilities.input_manager import InputManager #to read in fasta fil
 
 
 script_dir = Path( __file__ ).parent.absolute() #get the path to the dir where the test script is placed
-genomes = glob.glob(f"{script_dir}/../test_data/*.fna") #fetch the genome files
-
+genomes = glob.glob(f"{script_dir}/../test_data/human_*.fna") #fetch the genome files
 #set the paths for the output
 dir_path = f"{script_dir}/test_viruses"
 
@@ -42,59 +40,54 @@ dir_path = f"{script_dir}/test_viruses"
 if not Path(dir_path).exists():
     Path(dir_path).mkdir()
 
-ks = [x for x in range(3,34,3)] #create kmer size
+ks = [x for x in range(9,30,5)] #create kmer size
 
-def ham_dist(seq1, seq2):
+
+def hamming_distance(seq1, seq2):
     """
-    calculates the hamming distance
+    A rough estimation on how good the assembly is. The purpose is to show that a 100% correct sequence can be reconstructed
     """
     if len(seq1) == len(seq2):
-        hamming_distance = sum(nuc1 != nuc2 for nuc1,nuc2 in zip(seq1, seq2))
-        
-        portion_corr = 100*(len(seq1) - hamming_distance)/len(seq1)
-    else:
-        portion_corr = 100*(1-(len(seq2) - sum(nuc1 == nuc2 for nuc1,nuc2 in zip(seq1, seq2[:len(seq1)])))/len(seq2))
-        
-    return portion_corr
-
+        correct_pos = sum([n1 == n2 for n1, n2 in zip(seq1, seq2)]) #count the positions with a correct nucleotide      
+        return round(100*correct_pos/len(seq2),2) #return the percent matches
 
 for genome in genomes: #for each genome (fasta file)
     name = Path(genome).name
     for k in ks: #run all sizes of k
             
         output_file = f"{dir_path}/k_{k}_{name}"       
-        output_test_log = f"{dir_path}/log_k_{name}.txt"
+        output_test_log = f"{dir_path}/log_{name}.txt"
         
         
         with open(output_file, "w") as output_fasta, open(output_test_log, "a") as output_log: #open the output files
         
             sequence = "".join(InputManager(genome).read_fasta())
-            
-                       
-            
+                                             
             start = time.time() #get the current time
             solver = DbgSolver(genome, k) #call the solver
-            new_seq = solver.assemble() #get the reconstructed sequence 
+            assembly = solver.assemble() #get the reconstructed sequence 
             exe_time = round(time.time()-start,4) #get the execution time and poportion correctly assembled sequence
             
             name_id = genome.split("/")[-1] #format the header
             header = f"{name_id}_kmer_size:{k}"
             
             genome_size = len(sequence) #get the genome size for log info
-                       
-            #calc statistics            
-            poportion_correct = ham_dist(new_seq, sequence) 
+            assembly_size = len("".join(assembly))
             
-            #write the aseemblies to a file
-            output_fasta.write(">Scaffold_1") 
-            output_fasta.write("%s\n" % new_seq)
+            correct_bases = hamming_distance("".join(assembly), sequence)
+                            
+            #write the assemblies to a file
+            for i,seq in enumerate(assembly,1):
+                output_fasta.write(f">Scaffold_{i}\n") 
+                output_fasta.write("%s\n" % seq)
             
             
             #Write information to the log file
             output_log.write("Input genome: %s \n" % Path(genome).name) #first the input file
-            output_log.write("Genome size: %s \n" % genome_size) #the size of the genome
+            output_log.write("Reference size: %s \n" % genome_size) #the size of the genome
+            output_log.write("Assembly size: %s \n" % assembly_size) #the size of the genome
             output_log.write("kmer size used: %s \n" % k) #kmer size used
-            output_log.write("Poportion of sequence assembled correctly: %s%%\n" % poportion_correct) #poportion correct
+            output_log.write("Percentage correct nucleotides: %s \n" % correct_bases)
             output_log.write("Execution time: %s \n\n" % exe_time) #execution time
             
             
